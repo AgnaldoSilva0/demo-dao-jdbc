5dd11c000b9ef3f2d8d86d493c74bc9dd839acb6
Agnaldo Silva <agnaldo.santos24@hotmail.com>
2020-05-05 00:59:40 -0300

    findById implementation

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,20 +1,16 @@
 package aplication;
 
-import java.util.Date;
-
 import model_dao.DaoFactory;
 import model_dao.VendedorDao;
-import model_entities.Departamento;
 import model_entities.Vendedor;
 
 public class Program {
 
 	public static void main(String[] args) {
-
-		Departamento obj = new Departamento(1, "Livros");
-		Vendedor vendedor = new Vendedor(21, "Bob", "Bob@gamil.com", new Date(), 3000.0, obj);
 		
 		VendedorDao vendedorDao = DaoFactory.createVendedorDao();
+		
+		Vendedor vendedor = vendedorDao.findById(4);
 		
 		System.out.println(vendedor);
 		
