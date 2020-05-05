3511521f096d55cb77ca4a2a251f04c85d5bd7b9
Agnaldo Silva <agnaldo.santos24@hotmail.com>
2020-05-05 00:15:38 -0300

    Seller entity class

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,13 +1,17 @@
 package aplication;
 
+import java.util.Date;
+
 import model_entities.Departamento;
+import model_entities.Vendedor;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Departamento obj = new Departamento(1, "Livros");
-		System.out.println(obj);
+		Vendedor vendedor = new Vendedor(21, "Bob", "Bob@gamil.com", new Date(), 3000.0, obj);
+		System.out.println(vendedor);
 		
 	}
 
