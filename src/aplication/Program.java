8c747ac4289b1269c540c7b81f70ca4c7824d935
Agnaldo Silva <agnaldo.santos24@hotmail.com>
2020-05-05 00:31:16 -0300

    SellerDaoJDBC and DaoFactory

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -2,6 +2,8 @@ package aplication;
 
 import java.util.Date;
 
+import model_dao.DaoFactory;
+import model_dao.VendedorDao;
 import model_entities.Departamento;
 import model_entities.Vendedor;
 
@@ -11,7 +13,11 @@ public class Program {
 
 		Departamento obj = new Departamento(1, "Livros");
 		Vendedor vendedor = new Vendedor(21, "Bob", "Bob@gamil.com", new Date(), 3000.0, obj);
+		
+		VendedorDao vendedorDao = DaoFactory.createVendedorDao();
+		
 		System.out.println(vendedor);
+		
 		
 	}
 
---
+++ src/model/dao/impl/VendedorDaoJDBC.java
@@ -0,0 +1,39 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model_dao.VendedorDao;
+import model_entities.Vendedor;
+
+public class VendedorDaoJDBC implements VendedorDao {
+
+	@Override
+	public void insert(Vendedor obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Vendedor obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Vendedor findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Vendedor> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
