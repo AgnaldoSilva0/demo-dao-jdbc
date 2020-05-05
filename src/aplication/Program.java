7bff1f6a3e6e91d507ce715a3b981d31a560fe2f
Agnaldo Silva <agnaldo.santos24@hotmail.com>
2020-05-05 00:06:17 -0300

    Department entity class

---
+++ src/aplication/Program.java
@@ -0,0 +1,14 @@
+package aplication;
+
+import model_entities.Departamento;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Departamento obj = new Departamento(1, "Livros");
+		System.out.println(obj);
+		
+	}
+
+}
